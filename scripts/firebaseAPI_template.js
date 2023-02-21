3ccb96be0ee1cdbd9d15a7f7a6c5e0b2cc2d5028
sdsouza42 <dark.dsouza@hotmail.co.uk>
2023-02-21 14:44:13 -0800

    demo 8 done

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+  apiKey: "AIzaSyDoJbK3eb4Au8M4gL6xFKeYmeouER_EK3g",
+  authDomain: "comp1800-ds.firebaseapp.com",
+  projectId: "comp1800-ds",
+  storageBucket: "comp1800-ds.appspot.com",
+  messagingSenderId: "716600974574",
+  appId: "1:716600974574:web:7a39e2fe21196475b2a7ff"
 };
 
 //--------------------------------------------
